0fd9f88e63b2ffa409ac31625ff7ab3e2224ba31
nurzainpradana <pradananurzain@gmail.com>
2020-06-05 00:04:08 +0700

    Create Project, Create Directory, Import Drawable

---
+++ app/src/main/java/com/nurzainpradana/ecommercesepatu/ui/home/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.nurzainpradana.ecommercesepatu.ui.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nurzainpradana.ecommercesepatu.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
