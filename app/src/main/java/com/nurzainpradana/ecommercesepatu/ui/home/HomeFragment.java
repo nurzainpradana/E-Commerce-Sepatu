351f0c6107398d302bff2369e066e7bef8c3250f
nurzainpradana <pradananurzain@gmail.com>
2020-06-05 00:20:43 +0700

    create navigation home

---
+++ app/src/main/java/com/nurzainpradana/ecommercesepatu/ui/home/HomeFragment.java
@@ -0,0 +1,25 @@
+package com.nurzainpradana.ecommercesepatu.ui.home;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.nurzainpradana.ecommercesepatu.R;
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
