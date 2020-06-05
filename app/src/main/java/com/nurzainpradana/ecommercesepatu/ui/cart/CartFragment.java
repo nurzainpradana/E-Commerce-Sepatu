c1619acfa995adc887b5e6ebde17f62e52680fcb
nurzainpradana <pradananurzain@gmail.com>
2020-06-05 23:35:43 +0700

    finishing 19. Cart Page - Listener

--- app/src/main/java/com/nurzainpradana/ecommercesepatu/ui/cart/CartFragment.java
+++ app/src/main/java/com/nurzainpradana/ecommercesepatu/ui/cart/CartFragment.java
@@ -2,24 +2,24 @@ package com.nurzainpradana.ecommercesepatu.ui.cart;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.nurzainpradana.ecommercesepatu.R;
 
 public class CartFragment extends Fragment {
 
+    private Button btnCheckout;
 
     public CartFragment() {
         // Required empty public constructor
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
     }
 
     @Override
@@ -27,5 +27,24 @@ public class CartFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_cart, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        btnCheckout = view.findViewById(R.id.btn_checkout);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        btnCheckout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.action_cartFragment_to_shippingFragment);
+            }
+        });
     }
 }
--- app/src/main/java/com/nurzainpradana/ecommercesepatu/ui/cart/ShippingFragment.java
+++ app/src/main/java/com/nurzainpradana/ecommercesepatu/ui/cart/ShippingFragment.java
@@ -2,24 +2,23 @@ package com.nurzainpradana.ecommercesepatu.ui.cart;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.nurzainpradana.ecommercesepatu.R;
 
 public class ShippingFragment extends Fragment {
+    private Button btnGoToPayment;
 
     public ShippingFragment() {
         // Required empty public constructor
-    }
-
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
     }
 
     @Override
@@ -27,5 +26,24 @@ public class ShippingFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_shipping, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        btnGoToPayment = view.findViewById(R.id.btn_go_to_payment);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        btnGoToPayment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.action_shippingFragment_to_paymentFragment);
+            }
+        });
     }
 }
