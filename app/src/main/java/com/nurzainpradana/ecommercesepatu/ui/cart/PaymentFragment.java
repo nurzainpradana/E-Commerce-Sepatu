770c23d1ccdf77117dc7069a35f4173fe6bf7c19
nurzainpradana <pradananurzain@gmail.com>
2020-06-05 22:53:19 +0700

    finishing 17. Payment

---
+++ app/src/main/java/com/nurzainpradana/ecommercesepatu/ui/cart/PaymentFragment.java
@@ -0,0 +1,30 @@
+package com.nurzainpradana.ecommercesepatu.ui.cart;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.nurzainpradana.ecommercesepatu.R;
+
+public class PaymentFragment extends Fragment {
+
+    public PaymentFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_payment, container, false);
+    }
+}
