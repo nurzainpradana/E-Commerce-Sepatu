09d8684bcadf08fe4aa6a6ee8d741bb7d4b5b5a1
nurzainpradana <pradananurzain@gmail.com>
2020-06-05 15:06:07 +0700

    finishing 10. Cart Page - setting awal

---
+++ app/src/main/java/com/nurzainpradana/ecommercesepatu/ui/cart/CartActivity.java
@@ -0,0 +1,16 @@
+package com.nurzainpradana.ecommercesepatu.ui.cart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nurzainpradana.ecommercesepatu.R;
+
+public class CartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+}
---
+++ app/src/main/java/com/nurzainpradana/ecommercesepatu/ui/cart/CartFragment.java
@@ -0,0 +1,31 @@
+package com.nurzainpradana.ecommercesepatu.ui.cart;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.nurzainpradana.ecommercesepatu.R;
+
+public class CartFragment extends Fragment {
+
+
+    public CartFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cart, container, false);
+    }
+}
