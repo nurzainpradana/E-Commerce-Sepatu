a2a565605e1ae9412f73139b38a42ab407f9ee8b
nurzainpradana <pradananurzain@gmail.com>
2020-06-06 13:52:39 +0700

    finishing 23. Detail - Add View

---
+++ app/src/main/java/com/nurzainpradana/ecommercesepatu/ui/detail/DetailFragment.java
@@ -0,0 +1,30 @@
+package com.nurzainpradana.ecommercesepatu.ui.detail;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.nurzainpradana.ecommercesepatu.R;
+
+public class DetailFragment extends Fragment {
+
+    public DetailFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail, container, false);
+    }
+}
