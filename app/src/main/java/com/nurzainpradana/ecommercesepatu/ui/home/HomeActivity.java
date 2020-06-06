cc0119c57e41d9e55f067fa5a3e2564147a0c413
nurzainpradana <pradananurzain@gmail.com>
2020-06-06 12:34:19 +0700

    finishing 22. Home Page - add listener cart

--- app/src/main/java/com/nurzainpradana/ecommercesepatu/ui/home/HomeActivity.java
+++ app/src/main/java/com/nurzainpradana/ecommercesepatu/ui/home/HomeActivity.java
@@ -2,9 +2,13 @@ package com.nurzainpradana.ecommercesepatu.ui.home;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.nurzainpradana.ecommercesepatu.R;
+import com.nurzainpradana.ecommercesepatu.ui.cart.CartActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -12,5 +16,17 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        View viewToolbar = findViewById(R.id.layout_toolbar);
+        ImageView menuCart = viewToolbar.findViewById(R.id.menu_cart);
+
+        menuCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToCart = new Intent(HomeActivity.this, CartActivity.class);
+                startActivity(goToCart);
+            }
+        });
+
     }
 }
