ee1d5912085baf2c6e5ea92fec0eb3ec6eae5c9c
nurzainpradana <pradananurzain@gmail.com>
2020-06-10 17:32:22 +0700

    finishing 42. Home - Listener Category

--- app/src/main/java/com/nurzainpradana/ecommercesepatu/ui/categories/CategoriesActivity.java
+++ app/src/main/java/com/nurzainpradana/ecommercesepatu/ui/categories/CategoriesActivity.java
@@ -3,14 +3,22 @@ package com.nurzainpradana.ecommercesepatu.ui.categories;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.nurzainpradana.ecommercesepatu.R;
 
 public class CategoriesActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
+
+        View toolbar = findViewById(R.id.toolbar);
+        TextView titleToolbar = toolbar.findViewById(R.id.title_toolbar);
+        titleToolbar.setText("Categories");
     }
 }
--- app/src/main/java/com/nurzainpradana/ecommercesepatu/ui/home/HomeActivity.java
+++ app/src/main/java/com/nurzainpradana/ecommercesepatu/ui/home/HomeActivity.java
@@ -9,6 +9,7 @@ import android.widget.ImageView;
 
 import com.nurzainpradana.ecommercesepatu.R;
 import com.nurzainpradana.ecommercesepatu.ui.cart.CartActivity;
+import com.nurzainpradana.ecommercesepatu.ui.categories.CategoriesActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -19,11 +20,20 @@ public class HomeActivity extends AppCompatActivity {
 
         View viewToolbar = findViewById(R.id.layout_toolbar);
         ImageView menuCart = viewToolbar.findViewById(R.id.menu_cart);
+        ImageView menuFilter = viewToolbar.findViewById(R.id.menu_filter);
 
         menuCart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent goToCart = new Intent(HomeActivity.this, CartActivity.class);
+                startActivity(goToCart);
+            }
+        });
+
+        menuFilter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToCart = new Intent(HomeActivity.this, CategoriesActivity.class);
                 startActivity(goToCart);
             }
         });
