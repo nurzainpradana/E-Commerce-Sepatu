11d06410c5b272c3d3153d1fa16de6968485a169
nurzainpradana <pradananurzain@gmail.com>
2020-06-10 15:03:14 +0700

    finishing 38. Categories - Implementasi Adapter Categories

--- app/src/main/java/com/nurzainpradana/ecommercesepatu/ui/categories/CategoriesFragment.java
+++ app/src/main/java/com/nurzainpradana/ecommercesepatu/ui/categories/CategoriesFragment.java
@@ -2,15 +2,26 @@ package com.nurzainpradana.ecommercesepatu.ui.categories;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.nurzainpradana.ecommercesepatu.R;
+import com.nurzainpradana.ecommercesepatu.model.CategoriesModel;
 
-public class CategoriesFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoriesFragment extends Fragment implements CategoriesAdapter.ItemAdapterCallback{
+
+    private RecyclerView rvCategories;
+    private List<CategoriesModel> categoriesModelList;
 
     public CategoriesFragment() {
         // Required empty public constructor
@@ -26,5 +37,32 @@ public class CategoriesFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_categories, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        rvCategories = view.findViewById(R.id.rv_categories);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        categoriesModelList = new ArrayList<>();
+        categoriesModelList.add(new CategoriesModel(1, "All"));
+        categoriesModelList.add(new CategoriesModel(2, "Sport"));
+        categoriesModelList.add(new CategoriesModel(3, "Casual"));
+
+        CategoriesAdapter categoriesAdapter = new CategoriesAdapter(categoriesModelList, this);
+        rvCategories.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+        rvCategories.setAdapter(categoriesAdapter);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
