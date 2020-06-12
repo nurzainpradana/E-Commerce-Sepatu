d1a97aec3ca7527305c8123d4a9ad94748f43d2d
nurzainpradana <pradananurzain@gmail.com>
2020-06-12 16:30:40 +0700

    create API Service

---
+++ app/src/main/java/com/nurzainpradana/ecommercesepatu/api/ApiService.java
@@ -0,0 +1,22 @@
+package com.nurzainpradana.ecommercesepatu.api;
+
+import com.nurzainpradana.ecommercesepatu.model.cost.ItemCost;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface ApiService {
+
+    @FormUrlEncoded
+    @POST
+    Call<ItemCost> getCost(
+            @Field("key") String token,
+            @Field("android-key") String android,
+            @Field("origin") String origin,
+            @Field("destination") String destination,
+            @Field("weight") String weight,
+            @Field("courier") String courier
+    );
+}
